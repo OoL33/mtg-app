54916c493e15696d21671ef40311bc8e5c38b8b6
OoL33 <alanalee@tutanota.com>
2022-12-01 19:29:24 -0500

    configured deck GET endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	# get '/decks', to: 'homes#authenticated'
 	get '/users/:id', to: 'homes#authenticated'
-	get '/users/:user_id/decks/new', to: 'homes#authenticated'
-	get '/users/:user_id/decks/:id', to: 'decks#show'
+	# get '/users/:user_id/decks/new', to: 'homes#authenticated'
+	# get '/users/:user_id/decks/:id', to: 'decks#show'
 
 
 	# user dashboard
