044b9912740208598c3f04a13f8901266f456a2d
OoL33 <alanalee@tutanota.com>
2023-04-17 14:27:31 -0400

    Separated Scryfall API interactions to api_cards_controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::CardsController < ApiController
-	before_action :authenticate_user!
 
-	CLIENT = Faraday.new('https://api.magicthegathering.io/v1/cards')
+	before_action :authenticate_user!
 
 	def index
 		if params[:deck_id]
@@ -13,52 +12,56 @@ class Api::V1::CardsController < ApiController
 		render json: cards, each_serializer: CardSerializer
 	end
 
-	def search
-		search_string = params[:search_string]
-
-		client = Faraday.new('https://api.scryfall.com/cards/search')
-
-		response = client.get do |req|
-			req.params['q'] = search_string if search_string.present?
-		end
-		json_response = JSON.parse(response.body)
-
-		cards = json_response['data'].map do |card|
-			Card.find_or_create_by(external_ids: card['multiverse_ids']) do |c|
-				c.name = card['name']
-				c.colors = card['colors']
-				c.image_urls = card['image_uris']['normal'] if card['image_uris'].present?
-			end
-		end
-
-		filtered_cards = filter_cards(cards, params)
-
-		max_cards = 5
-
-		cards = cards.first(max_cards)
-
-		render json: cards.as_json
-	end
+  def search
+    # Delegate the Scryfall API querying and database interaction to ApiCardsController
+    api_cards_controller = Api::V1::ApiCardsController.new
+    api_cards_controller.search(params, filter_cards_method)
+  end
 
 	def create
 		deck = Deck.find_by(id: params[:deck_id])
-		card = Card.find_or_create_by(card_params)
+		if params[:card][:external_ids].present? # Add this conditional check
+			external_ids = params[:card][:external_ids].map(&:to_i)
+			cards = Card.where("external_ids && ARRAY[?]::integer[]", external_ids)
+		else
+			cards = []
+		end
 
-		#associated_cards = deck.cards.select { |obj| obj.id == card.id }
-
+		# Set api_cards variable by calling external API based on card name
+		search_string = params[:card][:name]
+		response = CLIENT.get('cards/search') do |req|
+			req.params['q'] = search_string if search_string.present?
+		end
+		json_response = JSON.parse(response.body)
+		api_cards = json_response['data']
+	
+		if cards.empty?
+			# Create a new card if it doesn't exist
+			card = Card.create(card_params.merge(external_ids: params[:card][:external_ids]))
+		else
+			# Search for card by name
+			card = cards.find { |c| c.name == params[:card][:name] }
+			if card.nil?
+				# Create a new card if card with the given name doesn't exist
+				card = Card.create(card_params.merge(external_ids: params[:card][:external_ids], name: params[:card][:name]))
+			end
+		end
+	
+		# Check if the card is already associated with the deck
 		if deck.cards.include?(card)
-			puts "\n\n card under id : #{card.id} is already associated with deck under id : #{deck.id} \n\n"
+			render json: { error: 'Card already associated with the deck' }, status: :unprocessable_entity
 		else
 			deck.cards << card
 			deck.save
 			render json: deck, status: :created
 		end
-	end	
+	end
+	
 
 	def show
 		deck = Deck.find( params[:deck_id])
-		cardsInDeck = deck.cards
-		render json: cardsInDeck, each_serializer: CardSerializer
+		cards_in_deck = deck.cards
+		render json: cards_in_deck, each_serializer: CardSerializer
 	end
 
 	def update
@@ -72,13 +75,13 @@ class Api::V1::CardsController < ApiController
 	private
 
 	def card_params
-		params.require(:card).permit(:id, :name, :colors, :image_urls, :external_ids)
+		params.require(:card).permit(:name, :colors, :image_urls, external_ids: [])
 	end
 
 	def filter_cards(cards, params)
 		cards = cards.filter { |card| card.name.include?(params[:name]) } if params[:name].present?
 		cards = cards.filter { |card| card.colors.include?(params[:color]) } if params[:color].present?
-		#cards = cards.filter { |card| card.image_urls.include?(params[:image_uris][:normal]) } if params[:image_uris].present?
+		cards = cards.filter { |card| card.image_urls.include?(params[:image_uris][:border_crop]) } if params[:image_uris].present?
 		cards
 	end
 end
