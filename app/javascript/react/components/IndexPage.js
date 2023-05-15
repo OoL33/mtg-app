554e43d5a46d09770fa3a35354ac18ca815940ab
OoL33 <alanalee@tutanota.com>
2023-05-15 09:36:06 -0400

    small styling to forms

--- app/javascript/react/components/IndexPage.js
+++ app/javascript/react/components/IndexPage.js
@@ -2,8 +2,12 @@ import React from "react"
 
 const IndexPage = () => {
 	return(
-		<div>
-			<h1>IndexPage.js</h1>
+		<div className="grid-container callout large">
+      <div className="index-page">
+      <h1>Welcome to MTG Deck Builder!</h1> 
+        <p>With this app, you can create, edit, and delete your own card decks for Magic the Gathering. Integrating with the Scryfall API allows you to search for and add any Magic the Gathering card to your deck with ease. You can also remove cards from your deck and make changes to it whenever you'd like.</p>
+        <p>Sign up now to start building your ultimate deck!</p>
+      </div>
 		</div>
 	)
 }
