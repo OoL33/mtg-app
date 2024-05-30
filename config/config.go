2f428dcf63add59eff2af248148927175f4e6047
Alana Lee <alanalee@tutanota.com>
2024-05-30 16:44:11 -0400

    Added godotenv

--- config/config.go
+++ config/config.go
@@ -3,7 +3,9 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,8 +13,21 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=your_username password=your_password dbname=breakable_toy_development port=5432 sslmode=disable"
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode)
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.23.0
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.10
